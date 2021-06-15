0229bb040c736e8f661b040a41e0114128d6b46b
091811075 Paola <PaolaGJH25@gmail.com>
2021-06-14 22:50:50 -0500

    Screen uno

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<marquee bgcolor="#006699" behavior="alternate" direction="right">
+<b><font color="#FFFFCC" size="5">BIENVENIDOS </font></b>
+</marquee>
+<br>
+</body>
+</html>
