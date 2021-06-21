a9538837b79c6b2e78e0d1efbb6771142d44a5de
091811075 Paola <PaolaGJH25@gmail.com>
2021-06-21 15:14:19 -0500

    Captura 2 del proyecto, modificación del index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <marquee bgcolor="#006699" behavior="alternate" direction="right">
-<b><font color="#FFFFCC" size="5">BIENVENIDOS </font></b>
+<b><font color="#FFFFCC" size="5">BIENVENIDOS =) </font></b>
 </marquee>
 <br>
 </body>
